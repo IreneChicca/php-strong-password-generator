cefbda02041d294b637046bb79143094a2b5c5cc
IreneChicca <irene.chicca@outlook.com>
2023-09-27 14:25:12 +0200

    Add static form

--- index.php
+++ index.php
@@ -6,9 +6,26 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <title>Document</title>
 </head>
 <body>
    
+<div class="w-50 p-5 m-auto border border-warning mt-3">
+
+<h3 class="mb-5">Inserisci la lunghezza della password:</h3>
+
+    <form action="">
+
+    <div class="input-group mb-3">
+  <input type="text" class="form-control" placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby="button-addon2">
+  <button class="btn btn-outline-secondary bg-warning text-black" type="button" id="button-addon2">Button</button>
+</div>
+
+    </form>
+
+</div>
+
+
 </body>
 </html>
